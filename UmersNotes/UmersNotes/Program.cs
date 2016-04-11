4719dd3621f76c7bb20beb4850f92a95008abc99
Umer Qurreshi <ume136@hotmail.com>
2016-04-11 17:24:56 +0100

    Add Umer's personal learning material. Parallel For and Foreach loops

---
+++ UmersNotes/UmersNotes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UmersNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>{10, 20};
+
+            Parallel.For(0, numbers.Count,
+                  index =>
+                  {
+                      int newNum = numbers[index];
+
+                      numbers.Add(newNum);
+                  });
+
+            Parallel.ForEach(numbers,
+              index => {
+                  Console.WriteLine(index);
+              });
+
+            Parallel.ForEach(numbers,  //Condensed version of the above foreach!!!
+                Console.WriteLine);
+
+            Console.ReadLine();
+        }
+    }
+}
