916c4b3d2b67e648987e7bba1cb1bc5181aa48fc
Umer Qurreshi <ume136@hotmail.com>
2016-04-13 10:33:12 +0100

    Show how to save word, excel and pdf files. Show how to automatically open a file

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 
@@ -9,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-#region Creating, saving, deleting files
+            #region Creating, saving, deleting files
 
             // Everything here we use in real life so if you learn this: MASSIVE step!!!
 
@@ -18,82 +19,111 @@ namespace ConsoleApplication1
             //value of 'file1' then go and find it by doing the below steps:
             // On right hand side in solution explorer, right click solution and click  
             // view folder in file explorer...Now find File1.txt...use value of file1 to help you!
-            var file1 = File.Create("File1.txt");
+            // var file1 = File.Create("File1.txt");
 
-            //Put break point on line 26 and run the application and see the error. 
-            //See what Path.Combine does?? It combines TWO paths into a single path. 
-            // Here we will get an error because once it
-            // hits the break point, it'll do xxxx\Folder2\File1.txt....therefore we first need to 
-            //create a folder (DIRECTORY) called Folder2.....so let's do this on line 37
+            // //Put break point on line 26 and run the application and see the error. 
+            // //See what Path.Combine does?? It combines TWO paths into a single path. 
+            // // Here we will get an error because once it
+            // // hits the break point, it'll do xxxx\Folder2\File1.txt....therefore we first need to 
+            // //create a folder (DIRECTORY) called Folder2.....so let's do this on line 37
 
-           // var file2 = File.Create(Path.Combine("Folder2", "File1.txt"));
+            //// var file2 = File.Create(Path.Combine("Folder2", "File1.txt"));
 
-            // Now, comment out above 2 lines of code and then check C-drive for a folder called
-            // Folder2 after you run the below  code...Note, the '@' removes the red 
-            //squiggly error we get because '\' is a special character which we must escape. Remove
-            // the '@' symbol and see for yourself
-            // Note how I am saying only create this folder (Directory) called Folder2 if it 
-            // DOES NOT EXIST.... !Directory.Exists(...) means if Directory NOT exist....therefore
-            // '!' means negating the if condition...so if it doesn't exist, only then go ahead
-            // and create it
+            // // Now, comment out above 2 lines of code and then check C-drive for a folder called
+            // // Folder2 after you run the below  code...Note, the '@' removes the red 
+            // //squiggly error we get because '\' is a special character which we must escape. Remove
+            // // the '@' symbol and see for yourself
+            // // Note how I am saying only create this folder (Directory) called Folder2 if it 
+            // // DOES NOT EXIST.... !Directory.Exists(...) means if Directory NOT exist....therefore
+            // // '!' means negating the if condition...so if it doesn't exist, only then go ahead
+            // // and create it
 
             if (!Directory.Exists(@"C:\Folder2"))
             {
                 Directory.CreateDirectory(@"C:\Folder2");
             }
 
-            // Now paste the line of code in line 26...var file2 =..... rename to var file3 =...
-            // Put break point on line 49 then press f10 and see the path. It'll save File1.txt into Folder2 in
-            // your c-drive. Use intellisense it hits break point to see value of file3 and really
-            // see what Path.Combine does. Converts two paths into a single path, so, you can say,
-            // C:\Folder2 is path 1 and File1.txt is path 2.....so Path.Combine will make this into
-            // C:\Folder2\File1.txt
-            var file3 = File.Create(Path.Combine(@"C:\Folder2", "File1.txt"));
+            // // Now paste the line of code in line 26...var file2 =..... rename to var file3 =...
+            // // Put break point on line 49 then press f10 and see the path. It'll save File1.txt into Folder2 in
+            // // your c-drive. Use intellisense it hits break point to see value of file3 and really
+            // // see what Path.Combine does. Converts two paths into a single path, so, you can say,
+            // // C:\Folder2 is path 1 and File1.txt is path 2.....so Path.Combine will make this into
+            // // C:\Folder2\File1.txt
+            // var file3 = File.Create(Path.Combine(@"C:\Folder2", "File1.txt"));
 
-            // Let's save a long line of text in File1.txt. 
-            File.WriteAllText(@"C:\Folder2\File1.txt", "Hey you are you ok?");
-            //As we can see, if our content is long, say a paragraph, then this line of code would 
-            // look horrible. For this reason when we want to add loads of strings, we use 
-            //stringbuilder. Let's use it.
+            // // Let's save a long line of text in File1.txt. 
+            // File.WriteAllText(@"C:\Folder2\File1.txt", "Hey you are you ok?");
+            // //As we can see, if our content is long, say a paragraph, then this line of code would 
+            // // look horrible. For this reason when we want to add loads of strings, we use 
+            // //stringbuilder. Let's use it.
 
-            StringBuilder builder = new StringBuilder();
-            builder.Append("Hi are you ok I am fine.").AppendLine("This will be on a new line");
-            File.WriteAllText(@"C:\Folder2\File1.txt", builder.ToString());
-            // Things to note about stringbuilder are that we can keep appending (adding strings) and 
-            // you see that in line 60, we simply pass in builder.ToString() as the second argument.
-            // Remember that 2nd argument in File.WriteAllText is the content which is of type string.
-            // However, our builder object is of type StringBuilder and that is why we do 
-            // builder.ToString() to convert from type StringBuilder to type string. Note that in
-            // StringBuilder I do .Append() then .AppendLine()...I do this because AppendLine() means
-            // it will automatically put the text onto the next line
+            // StringBuilder builder = new StringBuilder();
+            // builder.Append("Hi are you ok I am fine.").AppendLine("This will be on a new line");
+            // File.WriteAllText(@"C:\Folder2\File1.txt", builder.ToString());
+            // // Things to note about stringbuilder are that we can keep appending (adding strings) and 
+            // // you see that in line 60, we simply pass in builder.ToString() as the second argument.
+            // // Remember that 2nd argument in File.WriteAllText is the content which is of type string.
+            // // However, our builder object is of type StringBuilder and that is why we do 
+            // // builder.ToString() to convert from type StringBuilder to type string. Note that in
+            // // StringBuilder I do .Append() then .AppendLine()...I do this because AppendLine() means
+            // // it will automatically put the text onto the next line
 
-            // File.WriteAllLines, we have to append an Array of strings instead of just string.
-            // As you can see, in the second argument we pass in array of strings, in this case 2. 
-            // We can do Stringbuilder example with File.WriteAllLines. 
-            File.WriteAllLines(@"C:\Folder2\File1.txt", new string[] {"Umer is ok", "Adeeb is OK"});
+            // // File.WriteAllLines, we have to append an Array of strings instead of just string.
+            // // As you can see, in the second argument we pass in array of strings, in this case 2. 
+            // // We can do Stringbuilder example with File.WriteAllLines. 
+            // File.WriteAllLines(@"C:\Folder2\File1.txt", new string[] {"Umer is ok", "Adeeb is OK"});
 
-            StringBuilder builder1 = new StringBuilder();
-            builder1.Append("Umer is ok").AppendLine("Is Adeeb OK");
+            // StringBuilder builder1 = new StringBuilder();
+            // builder1.Append("Umer is ok").AppendLine("Is Adeeb OK");
 
-            StringBuilder builder2 = new StringBuilder();
-            builder2.Append("Is this good?").AppendLine("Surely it is not");
+            // StringBuilder builder2 = new StringBuilder();
+            // builder2.Append("Is this good?").AppendLine("Surely it is not");
 
-            File.WriteAllLines(@"C:\Folder2\File1.txt", new List<string> {builder1.ToString(), "Are we OK?", builder2.ToString()});
-            //Note above how we passed in 3 arguments: builder1.ToString(), Hardcoded string, builder2.ToString()
-            // Also note how we used List String instead of array. Can use either. So the above example
-            // as an array would be: 
-            File.WriteAllLines(@"C:\Folder2\File1.txt", new string[] { builder1.ToString(), "Are we OK?", builder2.ToString() });
+            // File.WriteAllLines(@"C:\Folder2\File1.txt", new List<string> {builder1.ToString(), "Are we OK?", builder2.ToString()});
+            // //Note above how we passed in 3 arguments: builder1.ToString(), Hardcoded string, builder2.ToString()
+            // // Also note how we used List String instead of array. Can use either. So the above example
+            // // as an array would be: 
+            // File.WriteAllLines(@"C:\Folder2\File1.txt", new string[] { builder1.ToString(), "Are we OK?", builder2.ToString() });
+
+            // // Now go repeat the above using File.AppendAllText() and File.AppendAllLines().
+            // // Remember that File.AppendAllText() is the same as File.WriteAllText() except that in
+            // // .AppendAllText() the existing content in the file is NOT overwritten. In .WriteAllText()
+            // // the content in the file IS overwritten. Again, File.AppendAllLines(), adds to existing
+            // // content in the file whereas File.WriteAllLines() replaces the content in the file. 
+            // // File.AppendAllLines() and File.WriteAllLines() expect an ARRAY/LIST of strings whereas
+            // // File.AppendAllText() and File.WriteAllText() expect a single string object
+
+
+            // Simply create Microsoft Word 97-2003 document
+            File.WriteAllText(@"C:\Folder2\Test.doc", "Hi word doc");
+
+            // Create a Microsoft word document (post 2007)
+            File.WriteAllText(@"C:\Folder2\Test1.docx", "Hi word doc");
+
+            // Create PDF document
+            File.WriteAllText(@"C:\Folder2\Test.pdf", "Hi PDF doc");
+
+            // Create 97-2003 Excel document
+            File.WriteAllText(@"C:\Folder2\Test2.xls", "Hi Excel doc");
+
+            // Create Excel document (post 2007)
+            File.WriteAllText(@"C:\Folder2\Test3.xlsx", "Hi Excel doc");
+
+            //Delete file if it exists
+            if (File.Exists(@"C:\Folder2\Test3.xlsx"))
+            {
+                File.Delete(@"C:\Folder2\Test3.xlsx");
+            }
+
+            // Automatically open a file! 
+            Process.Start(@"C:\Folder2\Test.doc");
             
-            // Now go repeat the above using File.AppendAllText() and File.AppendAllLines().
-            // Remember that File.AppendAllText() is the same as File.WriteAllText() except that in
-            // .AppendAllText() the existing content in the file is NOT overwritten. In .WriteAllText()
-            // the content in the file IS overwritten. Again, File.AppendAllLines(), adds to existing
-            // content in the file whereas File.WriteAllLines() replaces the content in the file. 
-            // File.AppendAllLines() and File.WriteAllLines() expect an ARRAY/LIST of strings whereas
-            // File.AppendAllText() and File.WriteAllText() expect a single string object
+          
+
+            
             string variableHereSoYouCanMovePastLine49BreakPointAndSeeTheValueInIntellisense = String.Empty;
 
-#endregion
+            #endregion
         }
     }
 }
