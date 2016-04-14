9d017f930ce5a587e49b5cad0379c2b31f777e07
Umer Qurreshi <ume136@hotmail.com>
2016-04-14 15:05:43 +0100

    Add angular js scripts

---
+++ WebApplication1/WebApplication1/js/app.js
@@ -0,0 +1,11 @@
+﻿angular.module('appEntryPoint', ['eventModule', 'ngResource'])
+.config([function ($httpProvider) {
+
+    /* configure providers not instances! */
+    console.log('Configuration hook');
+}])
+.run([function () {
+
+    /* run this when the app kicks off */
+    console.log('Run hook');
+}])
---
+++ WebApplication1/WebApplication1/js/eventModule.js
@@ -0,0 +1,47 @@
+﻿/* Declare angular module */
+angular.module('eventModule', [])
+
+/* Declare angular factory */
+.factory('emFactory', [function () {
+
+}])
+
+ /* Config - runs as soon as the app gets kicked off */
+.config([function () {
+
+    console.log('Event Module : config');
+}])
+
+/* If our module is up and running then execute the code inside the anonymous function */
+.run([function () {
+
+    console.log('Event Module : running')
+}])
+
+/* Controllers */
+
+.controller('ServiceController', ['$scope', '$resource', '$filter', function ($scope, $resource, $filter) {
+
+    var service = $resource("http://localhost:62100/api/values");
+    var fields = [];
+    var objects = [];
+    var objField = { fieldName: "txtFirstName" };
+    var dt = new Date("2011-09-07 14:43:22.520");
+
+    $scope.callPost = function () {
+
+        var data = {
+            id: "7",
+            changes: objects
+        };
+
+        service.save({}, data).$promise.then(
+            function (response) {
+                alert('Success');
+                console.log(response.name);
+            },
+            function () { alert('Error'); }
+        );
+    }
+
+}])
