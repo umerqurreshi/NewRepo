bbba68d3f048253bce0aea969318ebf5b34713a9
Umer Qurreshi <ume136@hotmail.com>
2016-04-14 15:35:43 +0100

    Add list of fulltime employees to fulltime employees class

--- Inheritance/Inheritance/Models/FulltimeEmployees.cs
+++ Inheritance/Inheritance/Models/FulltimeEmployees.cs
@@ -1,7 +1,48 @@
-﻿namespace Inheritance.Models
+﻿using System.Collections.Generic;
+
+namespace Inheritance.Models
 {
     public sealed class FulltimeEmployees : Employees
     {
         public string AnnualSalary { get; set; }
+
+        public List<FulltimeEmployees> GetAllEmployees()
+        {
+            var fte = FulltimeEmployeesRegister();
+
+            return fte;
+        }
+
+        private static List<FulltimeEmployees> FulltimeEmployeesRegister()
+        {
+            List<FulltimeEmployees> fte = new List<FulltimeEmployees>
+            {
+                new FulltimeEmployees
+                {
+                    Firstname = "Lucas",
+                    Lastname = "Jirto",
+                    Occupation = "Lawyer",
+                    AnnualSalary = "23000",
+                    Age = 32
+                },
+                new FulltimeEmployees
+                {
+                    Firstname = "James",
+                    Lastname = "Joy",
+                    Occupation = "Admin",
+                    AnnualSalary = "12500",
+                    Age = 27
+                },
+                new FulltimeEmployees
+                {
+                    Firstname = "Leyton",
+                    Lastname = "Kims",
+                    Occupation = "Doctor",
+                    AnnualSalary = "43000",
+                    Age = 35
+                }
+            };
+            return fte;
+        }
     }
 }
--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Inheritance
+﻿using System.Collections.Generic;
+using Inheritance.Models;
+
+namespace Inheritance
 {
     class Program
     {
         static void Main(string[] args)
         {
+            FulltimeEmployees fulltimeEmployee = new FulltimeEmployees
+            {
+            };
+
+            
         }
     }
 }
