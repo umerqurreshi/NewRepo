8eaef17bb6b14c8d9bd09a57429846794292a500
Umer Qurreshi <ume136@hotmail.com>
2016-04-14 14:56:56 +0100

    Get WebApi to return json instead of default XML. Remember to use fiddler/postman for API tests as opposed to the browser.

--- WebApplication1/WebApplication1/App_Start/WebApiConfig.cs
+++ WebApplication1/WebApplication1/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
@@ -25,6 +27,9 @@ namespace WebApplication1
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
